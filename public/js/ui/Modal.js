8f1ba857cfb4324cd0bdab071add01a8f3033b16
Biryukova Nadezhda <biryukova.nadezhda@bk.ru>
2022-03-17 23:02:25 +0300

    Класс Modal и sidebar

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -5,43 +5,53 @@
  * закрытие имеющихся окон
  * */
 class Modal {
-  /**
-   * Устанавливает текущий элемент в свойство element
-   * Регистрирует обработчики событий с помощью Modal.registerEvents()
-   * Если переданный элемент не существует,
-   * необходимо выкинуть ошибку.
-   * */
-  constructor(element){
+    /**
+     * Устанавливает текущий элемент в свойство element
+     * Регистрирует обработчики событий с помощью Modal.registerEvents()
+     * Если переданный элемент не существует,
+     * необходимо выкинуть ошибку.
+     * */
+    constructor(element) {
+        if (!element) {
+            return console.error("Элемент не задан");
+        } else {
+            this.element = element;
+            this.registerEvents();
+        }
+    }
 
-  }
+    /**
+     * При нажатии на элемент с data-dismiss="modal"
+     * должен закрыть текущее окно
+     * (с помощью метода Modal.onClose)
+     * */
+    registerEvents() {
+        let arrayClose = Array.from(this.element.querySelectorAll("dataAtr"));
+        arrayClose.forEach((item) => {
+            item.addEventListener("click", this.onClose());
+        })
+    }
 
-  /**
-   * При нажатии на элемент с data-dismiss="modal"
-   * должен закрыть текущее окно
-   * (с помощью метода Modal.onClose)
-   * */
-  registerEvents() {
+    /**
+     * Срабатывает после нажатия на элементы, закрывающие окно.
+     * Закрывает текущее окно (Modal.close())
+     * */
+    onClose(e) {
+        e.close();
+    }
 
-  }
+    /**
+     * Открывает окно: устанавливает CSS-свойство display
+     * со значением «block»
+     * */
+    open() {
+        this.element.style.display = "block";
+    }
 
-  /**
-   * Срабатывает после нажатия на элементы, закрывающие окно.
-   * Закрывает текущее окно (Modal.close())
-   * */
-  onClose(e) {
-
-  }
-  /**
-   * Открывает окно: устанавливает CSS-свойство display
-   * со значением «block»
-   * */
-  open() {
-
-  }
-  /**
-   * Закрывает окно: удаляет CSS-свойство display
-   * */
-  close(){
-
-  }
+    /**
+     * Закрывает окно: удаляет CSS-свойство display
+     * */
+    close() {
+        this.element.style.display = "";
+    }
 }
